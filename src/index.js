14ad5eb66865bdda42b9967a78d32ccb4b5f9001
Thomas Leconte <thomasl.dw20@gmail.com>
2024-02-06 12:22:14 +0100

    plugin react modal prêt pour un "npm publish"

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+export { default as Modal } from './modal';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
