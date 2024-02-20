8b4d404c3892d1e00f6843c3ca206f9ef1fd8ea7
Thomas Leconte <thomasl.dw20@gmail.com>
2024-02-20 17:17:35 +0100

    Remise à jour de la config webpack car ESModule renvoi des erreur, reprise avec require

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
-import { resolve as _resolve } from 'path';
-
-export const mode = 'production';
-export const entry = './src/index.js';
-export const output = {
-    path: _resolve(__dirname, 'dist'),
-    filename: 'my-modal-wh.js',
-    libraryTarget: 'umd',
-    library: 'my-modal-wh',
-    umdNamedDefine: true,
-};
-export const module = {
-    rules: [
-        {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-react'],
+const path = require('path');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js', 
+    output: {
+        path: path.resolve(__dirname, 'dist'), 
+        filename: 'my-modal-wh.js', 
+        libraryTarget: 'umd', 
+        library: 'my-modal-wh', 
+        umdNamedDefine: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', 
+                    options: {
+                        presets: ['@babel/preset-react'], 
+                    },
                 },
             },
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-                {
-                    loader: 'file-loader',
-                },
-            ],
-        },
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-    ],
-};
-export const resolve = {
-    extensions: ['.js', '.jsx'],
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], 
+    },
 };
